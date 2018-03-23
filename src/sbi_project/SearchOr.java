5609c4095ff7325f3054ba7ea53432d8bb6e040c
Mayra Azevedo <mayradazevedo@gmail.com>
2018-03-23 14:38:52 -0300

    refactoring code

--- src/sbi_project/SearchOr.java
+++ src/sbi_project/SearchOr.java
@@ -1,11 +1,10 @@
-package sbi_project;
-
-import java.util.ArrayList;
+package sbiproject;
 
 import Tree.Index;
 import Tree.Node;
 import Tree.TreeException;
 
+import java.util.ArrayList;
 
 /**
  * Classe filha (heranca) de Search
@@ -15,51 +14,51 @@ import Tree.TreeException;
  */
 public class SearchOr extends Search {
 
-	/**
-	 * Metodo constutor
-	 * @param db Banco de dados
-	 */
-	public SearchOr( DataBase db ){
-		super(db);
-	}
-	
-	/**
-	 * Metodo usado para realizar uma busca
-	 * @param data Palavra a ser buscada
-	 * @return Ocorrencia em que palavra(s)-chave foi encontrada
-	 * @throws TreeException
-	 */
-	@Override
-	public ArrayList<String> search(String data) throws TreeException {
-		this.occurrences.clear();
-		//Separa as palavras para serem buscadas
-		String [] words = data.split("\\s+");
-		
-		/**
-		 * Busca OR - lista todas as ocorrências das palavras buscadas,
-		 * caso uma palavra esteja em um arquivo, isto já é suficiente
-		 * para ela ser apresentada.
-		 */
-		ArrayList<Index> indexes = new ArrayList<Index>();
-		
-		// Busca as palavras 
-		for ( int i=0; i < words.length; i++) {
-			
-			Node node = db.searchNode(words[i]);
-			if ( node != null ) {
-				/**
-				 * Se uma palavra for encontrada então arquivar as ocorrencias dela.
-				 * Depois ordenar os índices de acordo com a quantidade de palavras 
-				 * encontradas nas linhas.
-				 */
-				indexes = busca.getIndexes();
-				this.sortResults(indexes, words[i]);
-			}
-		}
-		
-		return occurrences;
-	}
-	
-	
+  /**
+   * Metodo constutor.
+   * @param db Banco de dados
+   */
+  public SearchOr(DataBase db) {
+    super(db);
+  }
+  
+  /**
+   * Metodo usado para realizar uma busca.
+   * @param data Palavra a ser buscada
+   * @return Ocorrencia em que palavra(s)-chave foi encontrada
+   * @throws TreeException Exceção
+   */
+  @Override
+  public ArrayList<String> search(String data) throws TreeException {
+    this.occurrences.clear();
+    //Separa as palavras para serem buscadas
+    String [] words = data.split("\\s+");
+    
+    /**
+     * Busca OR - lista todas as ocorrências das palavras buscadas,
+     * caso uma palavra esteja em um arquivo, isto já é suficiente
+     * para ela ser apresentada.
+     */
+    ArrayList<Index> indexes = new ArrayList<Index>();
+    
+    // Busca as palavras 
+    for (int i = 0; i < words.length; i++) {
+      
+      Node node = db.searchNode(words[i]);
+      if (node != null) {
+        /**
+         * Se uma palavra for encontrada então arquivar as ocorrencias dela.
+         * Depois ordenar os índices de acordo com a quantidade de palavras 
+         * encontradas nas linhas.
+         */
+        indexes = busca.getIndexes();
+        this.sortResults(indexes, words[i]);
+      }
+    }
+    
+    return occurrences;
+  }
+  
+  
 }
-	
+  
--- src/sbi_project/SearchSystem.java
+++ src/sbi_project/SearchSystem.java
@@ -1,4 +1,4 @@
-package sbi_project;
+package sbiproject;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -15,42 +15,45 @@ import view.simpleSearchView;
  */
 public class SearchSystem {
 
-	// metodo main
-	public static void main(String[] args) {
+  /**
+   * Metodo main.
+   * @param args Argumentos
+   */
+  public static void main(String[] args) {
 
-		// instancia do banco de dados
-		DataBase db   = new DataBase();
-		// instancia da engine, passando o bd
-		Engine engine = new Engine( db );
-		
-		try {
-			FileInputStream fileIn = new FileInputStream(new File("./data/bd.ser").getCanonicalPath());
-			ObjectInputStream in = new ObjectInputStream(fileIn);
-			Engine e = (Engine) in.readObject();
-			engine = e;
-			db = engine.getDB();
-			in.close();
-			fileIn.close();
-		} catch( FileNotFoundException f) {
-			//Continua execução normalmente se o arquivo ainda n existe
-		} catch (IOException i) {
-			i.printStackTrace();
-			return;
-		} catch (ClassNotFoundException c) {
-			c.printStackTrace();
-			return;
-		}
+    // instancia do banco de dados
+    DataBase db   = new DataBase();
+    // instancia da engine, passando o bd
+    Engine engine = new Engine(db);
+    
+    try {
+      FileInputStream fileIn = new FileInputStream(new File("./data/bd.ser").getCanonicalPath());
+      ObjectInputStream in = new ObjectInputStream(fileIn);
+      Engine e = (Engine) in.readObject();
+      engine = e;
+      db = engine.getDB();
+      in.close();
+      fileIn.close();
+    } catch (FileNotFoundException f) {
+      //Continua execução normalmente se o arquivo ainda n existe
+    } catch (IOException i) {
+      i.printStackTrace();
+      return;
+    } catch (ClassNotFoundException c) {
+      c.printStackTrace();
+      return;
+    }
 
 
-		// instancia da busca "and", passando o bd
-		SearchAnd searchAnd = new SearchAnd( db );
-		// instancia da busca "or", passando o bd
-		SearchOr  searchOr  = new SearchOr( db );
-		// instancia da view, passando a engine, searchAnd e searchOr 
-		SimpleSearchView viewSbi = new simpleSearchView(engine, searchAnd, searchOr );
+    // instancia da busca "and", passando o bd
+    SearchAnd searchAnd = new SearchAnd(db);
+    // instancia da busca "or", passando o bd
+    SearchOr  searchOr  = new SearchOr(db);
+    // instancia da view, passando a engine, searchAnd e searchOr 
+    SimpleSearchView viewSbi = new simpleSearchView(engine, searchAnd, searchOr);
 
-		// exibe a tela inicial para usuario 
-		ViewSbi.setVisible(true);
-	}
+    // exibe a tela inicial para usuario 
+    ViewSbi.setVisible(true);
+  }
 
 }
